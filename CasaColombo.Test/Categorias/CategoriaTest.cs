7bbe43caca10f7b8f0a10cfebf620de280654916
vndamascena <vndamscena@gmail.com>
2025-02-28 17:54:35 -0300

    feat: implementado test de consulta a categoria e fornecedorGeral

---
+++ CasaColombo.Test/Categorias/CategoriaTest.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaColombo.Test.Categorias
+{
+    public class CategoriaTest
+    {
+        [Fact]
+        public void Test_Categorias_Get_Returns_Ok()
+        {
+            //fazendo uma chamada para o serviço de consulta de
+            //categorias da API
+            var client = new WebApplicationFactory<Program>()
+            .CreateClient();
+
+            var response = client.GetAsync("/api/Categoria").Result;
+            //verificando se o retorno da chamada é OK! (Http 200)
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ CasaColombo.Test/Fornecedor/FornecedorTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaColombo.Test.Fornecedor
+{
+    public class FornecedoresTest
+    {
+        [Fact]
+        public void Test_Fornecedores_Get_Returns_Ok()
+        {
+            //fazendo uma chamada para o serviço de consulta de
+            //fornecedores da API
+            var client = new WebApplicationFactory<Program>()
+
+            .CreateClient();
+
+            var response = client.GetAsync("/api/FornecedorGeral").Result;
+            //verificando se o retorno da chamada é OK! (Http 200)
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
